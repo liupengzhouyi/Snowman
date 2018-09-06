8c57503aa645e6223cfb1b7e496f0fda7d5b8e43
liupeng <LIUPENG.0@outlook.com>
2018-09-07 00:47:43 +0800

    This is a update for homework by chuyanhua, a good teacher who teach me java web, It's first homework,time : 2018-09-07 00:47

--- src/ServletPackage/liupengServlet.java
+++ src/ServletPackage/liupengServlet.java
@@ -11,8 +11,21 @@ import java.io.PrintWriter;
 @WebServlet(name = "liupengServlet")
 public class liupengServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter printWriter = response.getWriter();
-        printWriter.print("/ServletPackage/liupengServlet");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<table align='center' border='1'>");
+        out.println("<tr><th>参数名</tg><th>数值</th></tr>");
+        out.println("<tr><td>服务器IP</td><td>"+request.getRemoteAddr()+"</td></tr>");
+        out.println("<tr><td>服务器名</td><td>"+request.getServerName()+"</td></tr>");
+        out.println("<tr><td>服务器端口号</td><td>"+request.getServerPort()+"</td></tr>");
+        out.println("<tr><td>协议名</td><td>"+request.getScheme()+"</td></tr>");
+        out.println("<tr><td>协议版本</td><td>"+request.getProtocol()+"</td></tr>");
+        out.println("<tr><td>请求的方法</td><td>"+request.getMethod()+"</td></tr>");
+        out.println("<tr><td>请求的url</td><td>"+request.getRequestURI()+"</td></tr>");
+        out.println("<tr><td>上下文路径</td><td>"+request.getContextPath()+"</td></tr>");
+        out.println("<tr><td>Servlet路径</td><td>"+request.getServletPath()+"</td></tr>");
+        out.println("</table>");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

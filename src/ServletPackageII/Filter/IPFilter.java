52dcf76ffdef1a7d55073b586298d4f1fdd82049
liupeng <LIUPENG.0@outlook.com>
2018-10-23 17:58:25 +0800

    过滤器——修改完Bug V 1.0.1

--- src/ServletPackageII/Filter/IPFilter.java
+++ src/ServletPackageII/Filter/IPFilter.java
@@ -12,7 +12,7 @@ public class IPFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+
         //ServletRequest -> HttpServletRequest
         HttpServletRequest httpServletRequest = (HttpServletRequest) req;
         //ServletResponse -> HttpServletResponse
@@ -21,31 +21,55 @@ public class IPFilter implements Filter {
         String strStartIp = this.filterConfig.getInitParameter("startIP");
         String strEndIp = this.filterConfig.getInitParameter("endIP");
 
-        httpServletRequest.setAttribute("strstarIp", strStartIp);
+        httpServletRequest.setAttribute("strStarIp", strStartIp);
         httpServletRequest.setAttribute("strEndIp", endIp);
 
         this.setStartIp(Integer.parseInt(strStartIp.replace(".", "")));
         this.setEndIp(Integer.parseInt(strEndIp.replace(".", "")));
 
+        System.out.println(this.getStartIp());
+        System.out.println(this.getEndIp());
+
         //获取客户端IP
-        String reqIp = req.getRemoteHost();
+        //String reqIp = req.getRemoteHost();
+        String reqIp = req.getRemoteAddr();
+
+        if (reqIp.equals("0:0:0:0:0:0:0:1")) {
+            reqIp = "127.0.0.1";
+        } else {
+            System.out.println("-------");
+        }
+
+        System.out.println(reqIp);
 
         req.setAttribute("reqIp", reqIp);
 
         reqIp = reqIp.replace(".", "");
         int ip = Integer.parseInt(reqIp);
 
-        if (ip>endIp || ip <startIp) {
-            System.out.println("转到错误页面！");
-            ((HttpServletResponse) resp).sendRedirect("/test/测试页面重定向/Hello.jsp");
+        System.out.println("IP:" + ip);
+
+        if (ip != startIp) {
+            System.out.println("开始使用您的IP");
+            chain.doFilter(req, resp);
         } else {
-            System.out.println("您的IP允许访问！");
+            //((HttpServletResponse) resp).sendRedirect("/paly.jsp");
+            System.out.println("转到错误页面！");
+            req.getRequestDispatcher("/paly.jsp").forward(req, resp);
         }
 
+       /* if (ip>endIp || ip <startIp) {
+            System.out.println("转到错误页面！");
+            req.getRequestDispatcher("/test/测试页面重定向/Hello.jsp").forward(req, resp);
+        } else {
+            System.out.println("您的IP允许访问！");
+        }*/
+        //chain.doFilter(req, resp);
     }
 
     public void init(FilterConfig config) throws ServletException {
         this.filterConfig = config;
+        System.out.println("开始使用过滤器");
     }
 
 
